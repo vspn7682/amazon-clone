6a5a3162291363e7f1a29e9b0b35c5812ed00e7d
Shailesh <vspn8569@gmail.com>
2021-03-04 11:28:16 +0530

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.hero').slick({
+        // autoplay: true,
+        // autoplaySpeed: 5000,
+        prevArrow: '.prev',
+        nextArrow: '.next'
+    })
+})
